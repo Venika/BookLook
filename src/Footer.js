15607fe7918ab2646b98b4ee9a665550a1097931
Venika Gaur <Venika.gaur2610@gmail.com>
2020-11-09 19:04:05 -0600

    Media query css

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <footer className="copyright">
-                <p>&copy; 2020 <a href="#">Venika Gaur</a>
+                <p>&copy; 2020 Venika Gaur
                  <a href="https://github.com/Venika" className="social" target="_blank">
                         <i className="fab fa-github"></i>
                     </a>
